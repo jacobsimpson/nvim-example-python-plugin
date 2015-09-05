6407e0d3a7ae687fe5e6a9e045b162adb6f8c196
Jacob Simpson <jacob.simpson@gmail.com>
2015-09-05 08:55:34 -0700

    Simplified Python code file structure.

--- rplugin/python/nvim-example-python-plugin.py
+++ rplugin/python/nvim-example-python-plugin.py
@@ -1,1 +1,11 @@
-Autocmd: Called file: nvim-example-python-plugin.py
+import neovim
+
+@neovim.plugin
+class Main(object):
+    def __init__(self, vim):
+        self.vim = vim
+
+    @neovim.function('DoItPython')
+    def doItPython(self, args):
+        self.vim.command('echo "hello"')
+
